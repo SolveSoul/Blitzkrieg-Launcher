309b13970af03be67466626fe4d071dbc158c6a1
SolveSoul <solvesoul@gmail.com>
2014-09-22 13:28:47 +0200

    Added AI file handling

---
+++ BlitzkriegLauncher/Helpers/AIFileHandler.cs
@@ -0,0 +1,37 @@
+﻿using BlitzkriegLauncher.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlitzkriegLauncher.Helpers
+{
+    public class AIFileHandler
+    {
+        private static string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+        public static List<AIFile> GetActiveAIFiles() 
+        {
+            List<AIFile> result = new List<AIFile>();
+
+            foreach(string filename in Directory.GetFiles(baseDir))
+                if (filename.Contains("ailogic")) 
+                    result.Add(CreateAIFile(filename));
+
+            return result;
+        }
+
+        private static AIFile CreateAIFile(string filename)
+        {
+            AIFile result = new AIFile(filename);
+            
+
+
+
+            return result;
+        }
+
+    }
+}
---
+++ BlitzkriegLauncher/Models/AIFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlitzkriegLauncher.Models
+{
+    public class AIFile
+    {
+        //props
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public long FileSize { get; set; }
+        public string FileExtension { get; set; }
+
+        //ctor
+        public AIFile(string fullPath)
+        {
+            this.FileSize = GetFileSize(fullPath);
+            this.FileExtension = GetFileExtension(fullPath);
+        }
+
+        //getters & setters
+        public long GetFileSize(string fullPath) 
+        {
+            if (File.Exists(fullPath))
+                return new FileInfo(fullPath).Length;
+            else
+                return 0;
+        }
+
+        public string GetFileExtension(string fullPath) 
+        {
+            if (File.Exists(fullPath))
+                return new FileInfo(fullPath).Extension;
+            else
+                return String.Empty;
+        }
+    }
+}
