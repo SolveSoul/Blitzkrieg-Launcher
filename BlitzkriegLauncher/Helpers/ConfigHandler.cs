b00b60614e37d9632cf9553079e3c8df669cad72
SolveSoul <solvesoul@gmail.com>
2014-09-22 21:54:11 +0200

    Updated UI + configfilehandler

---
+++ BlitzkriegLauncher/Helpers/ConfigHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BlitzkriegLauncher.Helpers
+{
+    public class ConfigHandler
+    {
+
+        private const string configFile = AppDomain.CurrentDomain.BaseDirectory + "config.cfg";
+
+        public static void ReadConfigFile() 
+        {
+            if (File.Exists(configFile))
+            {
+
+            }
+            else
+                MessageBox.Show("Could not find the 'config.cfg' file.", "File not found", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+    }
+}
